8aaaaa9f26c9bf2227fd6e25fb27b907145343f6
ppkds <ppkds@outlook.com>
2021-08-14 17:54:25 -0500

    Tweaks

--- handystuff/whatismyip.sh
+++ handystuff/whatismyip.sh
@@ -5,23 +5,21 @@
 # Updated: Sat Aug 14 17:04:36 CDT 2021                     #
 #############################################################
 
-# Get external IP
+# Get external IP 
+# Other sites | https://checkip.amazonaws.com | https://ipinfo.io/ip | 
 externalip=$(curl -s https://icanhazip.com)
 # Get internal IP
 # Internalip=$(ifconfig | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2 "/" $4}')	# Use this to display subnet mask
 internalip=$(hostname -I) 																# Use this to display without subnet mask
 # Get Gateway IP - updated: Mon Sep 03 11:59:41 CDT 2018
-routerip=$(ip r | grep 'default' | awk '{print "Router IP: " $3}')
+routerip=$(ip r | grep 'default' | awk '{print "Gateway: " $3}')
 # Get Conneclient device - updated: Sat Aug 14 17:04:36 CDT 2021
-connectedclientdevice=$(ip r | grep 'default' | awk '{print "Connected Device(s): " $5}')
+connectedclientdevice=$(ip r | grep 'default' | awk '{print $5}')
 
 # Echo output to stdout
 echo
-echo "IP Connection"
-echo "-------------"
+echo "IP Connection | ${connectedclientdevice} | ${routerip}"
 echo
-echo "${connectedclientdevice}"
-echo "${routerip}"
 echo "Internal IP":" ${internalip}"
 echo "External IP":" ${externalip}"
 echo
