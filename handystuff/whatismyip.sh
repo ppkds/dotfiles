8eea8ec8aa815fdd1ab7fcf76f372aaea631e776
ppkds <ppkds@outlook.com>
2021-12-21 20:05:33 -0600

    Tweak: optimized

--- handystuff/whatismyip.sh
+++ handystuff/whatismyip.sh
@@ -11,15 +11,17 @@ externalip=$(curl -s https://icanhazip.com)
 # Internalip=$(ifconfig | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2 "/" $4}')	# Use this to display subnet mask
 internalip=$(hostname -I) 																# Use this to display without subnet mask
 # Get Gateway IP - updated: Mon Sep 03 11:59:41 CDT 2018
-routerip=$(ip r | grep 'default' | awk '{print "Gateway: " $3}')
+routerip=$(ip r | grep 'default' | awk '{print $3}')
 # Get Connected client device - updated: Sat Aug 14 17:04:36 CDT 2021
 connectedclientdevice=$(ip r | grep 'default' | awk '{print $5}')
 
 # Echo output to stdout
 echo
-echo "Connection | ${connectedclientdevice} | ${routerip}"
-echo
+echo --------
+echo "Connection: ${connectedclientdevice} || Gateway: ${routerip}"
+echo --------
 echo "Internal IP":" ${internalip}"
 echo "External IP":" ${externalip}"
+echo --------
 echo
 exit 0
