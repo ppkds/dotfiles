402d35f44c5f2025d8846e6e76ddf3af952844e2
ppkds <ppkds@outlook.com>
2022-12-25 12:39:32 -0600

    replaced deprecated 'ipconfig' with 'ip r'

--- handystuff/whatismyip.sh
+++ handystuff/whatismyip.sh
@@ -8,8 +8,9 @@
 # Other sites | https://checkip.amazonaws.com | https://ipinfo.io/ip |
 externalip=$(curl -s https://icanhazip.com)
 # Get internal IP
-# Internalip=$(ifconfig | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2 "/" $4}')	# Use this to display subnet mask
-internalip=$(hostname -I) 																# Use this to display without subnet mask
+# Deprecated: internalip=$(ifconfig | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2 "/" $4}')	# Use this to display subnet mask
+# Deprecated: internalip=$(hostname -I) 															# Use this to display w/O subnet mask
+internalip=$(ip r | grep 'scope' | awk '{print $9}')
 # Get Gateway IP - updated: Mon Sep 03 11:59:41 CDT 2018
 routerip=$(ip r | grep 'default' | awk '{print $3}')
 # Get Connected client device - updated: Sat Aug 14 17:04:36 CDT 2021
