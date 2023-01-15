e1d1ea10bfc779922236d98ee6074a060c7c5e6d
ppkds <ppkds@outlook.com>
2023-01-14 21:31:34 -0600

    Tweak: added subnet mask

--- handystuff/whatismyip.sh
+++ handystuff/whatismyip.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 # +-----------------------------------------+
 # |Created: Thu Aug 06 23:46:13 CST 2015    |
-# |Modified: Tue Dec 21 20:07:52 CST 2021   |   
 # +-----------------------------------------+
 
-# Get external IP
-# Other sites | https://checkip.amazonaws.com | https://ipinfo.io/ip |
-externalip=$(curl -s https://icanhazip.com)
-# Get internal IP
-# Deprecated: internalip=$(ifconfig | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2 "/" $4}')	# Use this to display subnet mask
-# Deprecated: internalip=$(hostname -I) 															# Use this to display w/O subnet mask
-internalip=$(ip r | grep 'scope' | awk '{print $9}')
-# Get Gateway IP - updated: Mon Sep 03 11:59:41 CDT 2018
+# Get Gateway IP
 routerip=$(ip r | grep 'default' | awk '{print $3}')
-# Get Connected client device - updated: Sat Aug 14 17:04:36 CDT 2021
+# Get subnet mask from router
+subnetmask=$(ip r | grep 'scope' | awk '{print $1}')
+# Get internal IP
+internalip=$(ip r | grep 'scope' | awk '{print $9}')
+# Get external IP
+externalip=$(curl -s https://icanhazip.com) # Other sites | https://checkip.amazonaws.com | https://ipinfo.io/ip |
+# Get Connected client device
 clientdevice=$(ip r | grep 'default' | awk '{print $5}')
 
 # Echo output to stdout
 echo
 echo "Gateway IP    : ${routerip}"
+echo "Subnet mask   : ${subnetmask}"
 echo "Internal IP   : ${internalip}"
 echo "External IP   : ${externalip}"
 echo "Interface     : ${clientdevice}"
-echo
 exit 0
