bfea0a76db4ecd363bd19f142adca89912a766d2
ppkds <ppkds@outlook.com>
2023-01-15 03:04:15 -0600

    Tweak

--- handystuff/whatismyip.sh
+++ handystuff/whatismyip.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-# +-----------------------------------------+
-# |Created: Thu Aug 06 23:46:13 CST 2015    |
-# +-----------------------------------------+
 
-# Get Gateway IP
+# Created Aug 06, 2015 - PPKDS
+
+# Gateway IP
 routerip=$(ip r | grep 'default' | awk '{print $3}')
-# Get subnet mask from router
+# Subnet mask from router
 subnetmask=$(ip r | grep 'scope' | awk '{print $1}')
-# Get internal IP
+# Internal IP
 internalip=$(ip r | grep 'scope' | awk '{print $9}')
-# Get external IP
-externalip=$(curl -s https://icanhazip.com) # Other sites | https://checkip.amazonaws.com | https://ipinfo.io/ip |
-# Get Connected client device
+# External IP
+externalip=$(curl -s https://icanhazip.com) # Alternative: https://checkip.amazonaws.com; https://ipinfo.io/ip
+# Connected client device
 clientdevice=$(ip r | grep 'default' | awk '{print $5}')
 
 # Echo output to stdout
 echo "Gateway IP    : ${routerip}"
-echo "Subnet mask   : ${subnetmask}"
+echo "Subnet/mask   : ${subnetmask}"
 echo "Internal IP   : ${internalip}"
 echo "External IP   : ${externalip}"
 echo "Interface     : ${clientdevice}"
