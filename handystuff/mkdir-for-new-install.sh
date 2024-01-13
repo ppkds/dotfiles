0a0217e81e776fe0d162f896aee55e64b17d3836
PPKDS <54788686+ppkds@users.noreply.github.com>
2024-01-12 21:41:34 -0600

    Update mkdir-for-new-install.sh

--- handystuff/mkdir-for-new-install.sh
+++ handystuff/mkdir-for-new-install.sh
@@ -1,1 +1,1 @@
-mkdir -pv Desktop Dev Documents Documents/Original_configs Dev/Git Dev/Git/dotfiles Dev/Practice Dev/Scripts Downloads Music Pictures Public Templates Videos tempdir .config/bat .config/btop .config/htop .config/mc .config/micro .config/neofetch .vim
+mkdir -pv Desktop Dev Documents Documents/Original_configs Dev/Git Dev/Git/dotfiles Dev/Practice Dev/Scripts Downloads Music Pictures Public Templates Videos tempdir .config/bat .config/btop .config/htop .config/mc .config/micro .config/neofetch .fonts .vim
