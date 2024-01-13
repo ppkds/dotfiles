ae3594a2e60bb0e10f067ee6eca2fe6ed1d2f833
PPKDS <54788686+ppkds@users.noreply.github.com>
2024-01-12 18:41:18 -0600

    Update mkdir-for-new-install.sh
    added directory for original scripts

--- handystuff/mkdir-for-new-install.sh
+++ handystuff/mkdir-for-new-install.sh
@@ -1,1 +1,1 @@
-mkdir -pv Desktop Dev Documents Dev/Git Dev/Git/dotfiles Dev/Practice Dev/Scripts Downloads Music Pictures Public Templates Videos tempdir .config/bat .config/htop .config/mc .config/micro .config/neofetch .nano .vim
+mkdir -pv Desktop Dev Documents Documents/Original_configs Dev/Git Dev/Git/dotfiles Dev/Practice Dev/Scripts Downloads Music Pictures Public Templates Videos tempdir .config/bat .config/htop .config/mc .config/micro .config/neofetch .nano .vim
