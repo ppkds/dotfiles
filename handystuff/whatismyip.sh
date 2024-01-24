6f814d4388e78f195fcd2889656dad6a20b092b6
PPKDS <ppkds@outlook.com>
2024-01-23 19:05:34 -0600

    Tweaked IP display

--- handystuff/whatismyip.sh
+++ handystuff/whatismyip.sh
@@ -7,7 +7,7 @@ routerip=$(ip r | grep 'default' | awk '{print $3}')
 # Subnet mask from router
 subnetmask=$(ip r | grep 'scope' | awk '{print $1}')
 # Internal IP
-internalip=$(ip r | grep 'scope' | awk '{print $9}')
+internalip=$(ip r | grep 'scope' | grep -Fv 'vir' | awk '{print $9}')
 # External IP
 externalip=$(curl -s https://icanhazip.com) # Alternatives: https://checkip.amazonaws.com; https://ipinfo.io/ip; https://ident.me
 # Connected client device
@@ -16,7 +16,7 @@ clientdevice=$(ip r | grep 'default' | awk '{print $5}')
 # Echo output to stdout
 echo
 echo "Gateway IP    : ${routerip}"
-echo "Subnet/mask   : ${subnetmask}"
+# echo "Subnet/mask   : ${subnetmask}"
 echo "Internal IP   : ${internalip}"
 echo "External IP   : ${externalip}"
 echo "Interface     : ${clientdevice}"
