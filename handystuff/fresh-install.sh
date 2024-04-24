944593b26ca229456356cc7bdbeb21444d0d6a48
PPKDS <ppkds@outlook.com>
2024-04-24 13:48:29 -0500

    Tweaks

--- handystuff/fresh-install.sh
+++ handystuff/fresh-install.sh
@@ -3,7 +3,7 @@
 # Script for fresh Debian based installs.
 
 # Create user specific directories after fresh install of Debian based linux
-mkdir -pv Desktop Dev Dev/Git Dev/Git/dotfiles Dev/Practice Dev/Scripts Documents Documents/Original_configs Downloads Music Pictures Public Templates Videos tempdir .config/bat .config/btop .config/htop .config/mc .config/micro .config/nano .config/neofetch .fonts .vim
+cd && mkdir -pv Desktop Dev Dev/Git Dev/Git/dotfiles Dev/Practice Dev/Scripts Documents Documents/Original_configs Downloads Music Pictures Public Templates Videos tempdir .config/bat .config/btop .config/htop .config/mc .config/micro .config/nano .config/neofetch .fonts .vim
 
 # Update, upgrade and install useful programs.
-sudo apt update && echo && echo "Following programs to be upgraded:" && echo && sudo apt list --upgradeable && echo && read -p "Press any key to continue: " -n 1 -s && echo && sudo apt dist-upgrade -y && echo && echo "Programs not upgradable above are 'not in band' and likely will be upgraded during a subsequent upgrade session" && echo && read -p "Press any key to install useful utilities or ^C to cancel: " -n 1 -s && sudo apt install bat btop cheese curl exa git gnupg2 gparted htop imagemagick mc micro mplayer neofetch pwgen qemu tree vim wget -y
+cd && sudo apt update && echo && echo "Following programs to be upgraded:" && echo && sudo apt list --upgradeable && echo && read -p "Press any key to continue: " -n 1 -s && echo && sudo apt dist-upgrade -y && echo && echo "Programs not upgradable above are 'not in band' and likely will be upgraded during a subsequent upgrade session" && echo && read -p "Press any key to install useful utilities or ^C to cancel: " -n 1 -s && sudo apt install bat btop cheese curl exa git gnupg2 gparted htop imagemagick mc micro mplayer neofetch pwgen qemu tree vim wget -y
