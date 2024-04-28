58d4a54e2dd664e2605364e4d50308df4f1838c7
PPKDS <ppkds@outlook.com>
2024-04-28 09:43:34 -0500

    Deleted and combined script into one @ fresh-install.sh

--- handystuff/install-apps.sh
+++
@@ -1,1 +0,0 @@
-sudo apt update && sudo apt install batcat btop cheese curl exa git gnupg2 gparted htop imagemagick mc micro mplayer neofetch qemu tree vim wget
--- handystuff/mkdir-for-new-install.sh
+++
@@ -1,1 +0,0 @@
-mkdir -pv Desktop Dev Documents Documents/Original_configs Dev/Git Dev/Git/dotfiles Dev/Practice Dev/Scripts Downloads Music Pictures Public Templates Videos tempdir .config/bat .config/btop .config/htop .config/mc .config/micro .config/nano .config/neofetch .fonts .vim
